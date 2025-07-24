fca99820d168ae1348c1e45ab863d486e717419b
schooli987 <curriculumfiles@schooli.online>
2025-07-24 13:18:43 +0530

    Update PCP_template.py

--- PCP_template.py
+++ PCP_template.py
@@ -1,4 +1,4 @@
-# Sample racks (you can later ask students to input these)
+# Sample racks (input given in the form of List.you can later try for different input)
 small_rack = [4, 5]              # Small-sized balls
 medium_rack = [6, 7, 6]          # Medium-sized balls
 large_rack = [8]                 # Large-sized balls
